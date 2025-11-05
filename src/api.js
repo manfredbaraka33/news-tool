872a5fd1fd7660a8ad087555f16644340cc1ddc4
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2025-11-05 20:46:50 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "https://elopyx-news-tool.hf.space";
+const API_BASE = "https://news-tool-backend-production.up.railway.app/";
 
 export const processUrls = (urls) => axios.post(`${API_BASE}/process/`, { urls });
 export const askQuestion = (query) => axios.post(`${API_BASE}/ask/`, { query });
